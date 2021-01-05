f6462e448f8a1b7e3053ac594f768c4a4c23f8b3
Francisco Brito <francisco.brito.ac@gmail.com>
2021-01-05 07:20:18 -0300

    POST /orders

--- backend/src/main/java/com/facbrito/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/facbrito/dsdeliver/controllers/OrderController.java
@@ -1,12 +1,16 @@
 package com.facbrito.dsdeliver.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.facbrito.dsdeliver.dto.OrderDTO;
 import com.facbrito.dsdeliver.services.OrderService;
@@ -14,14 +18,21 @@ import com.facbrito.dsdeliver.services.OrderService;
 @RestController
 @RequestMapping(value = "/orders")
 public class OrderController {
-	
+
 	@Autowired
 	private OrderService orderService;
-	
+
 	@GetMapping
 	public ResponseEntity<List<OrderDTO>> findAll() {
 		List<OrderDTO> orderDTOS = orderService.findAll();
 		return ResponseEntity.ok().body(orderDTOS);
+	}
+
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO orderDTO) {
+		orderDTO = orderService.insert(orderDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(orderDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(orderDTO);
 	}
 
 }
--- backend/src/main/java/com/facbrito/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/facbrito/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.facbrito.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.facbrito.dsdeliver.dto.OrderDTO;
+import com.facbrito.dsdeliver.dto.ProductDTO;
 import com.facbrito.dsdeliver.entities.Order;
+import com.facbrito.dsdeliver.entities.OrderStatus;
+import com.facbrito.dsdeliver.entities.Product;
 import com.facbrito.dsdeliver.repositories.OrderRepository;
+import com.facbrito.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,25 @@ public class OrderService {
 	@Autowired
 	private OrderRepository orderRepository;
 
+	@Autowired
+	private ProductRepository productRepository;
+
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> orders = orderRepository.findOrdersWithProducts();
 		return orders.stream().map(p -> new OrderDTO(p)).collect(Collectors.toList());
+	}
+
+	@Transactional
+	public OrderDTO insert(OrderDTO orderDTO) {
+		Order order = new Order(null, orderDTO.getAddress(), orderDTO.getLatitude(), orderDTO.getLongitude(),
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : orderDTO.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+
+		order = orderRepository.save(order);
+		return new OrderDTO(order);
 	}
 }
